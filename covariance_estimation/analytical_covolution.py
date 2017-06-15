ffba867d210c138b2cc370d76b46764b19e4b507
Sreekumar Thaithara Balan <tbs1980@gmail.com>
2017-06-15 22:30:24 +0100

    analytical convolution tested with jeffreys prior

--- covariance_estimation/analytical_covolution.py
+++ covariance_estimation/analytical_covolution.py
@@ -1,14 +1,26 @@
-from math import log, exp
+from math import (log, exp, sqrt)
 import numpy as np
 
 
 def log_posterior(omega, zeta, sum_y2, n):
-    if omega < 0.:
+    if omega <= 0.:
         return -np.inf
     return -0.5*n*log(zeta + omega) - 0.5*sum_y2/(zeta + omega)
 
 
 def posterior(omega, zeta, sum_y2, n):
-    if omega < 0.:
+    if omega <= 0.:
         return 0.
     return exp(log_posterior(omega, zeta, sum_y2, n))
+
+
+def log_posterior_jeffreys_prior(omega, zeta, sum_y2, n):
+    if omega <= 0.:
+        return -np.inf
+    return log_posterior(omega, zeta, sum_y2, n) - 0.5*log(omega)
+
+
+def posterior_jeffreys_prior(omega, zeta, sum_y2, n):
+    if omega <= 0.:
+        return np.inf
+    return posterior(omega, zeta, sum_y2, n)/sqrt(omega)
